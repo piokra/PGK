ddc4600dc7be59643e9b50a5134f3cd7ed3e78a5
piokra <piotr.krasun@gmail.com>
2018-02-28 21:07:35 +0100

    flickering

--- KupaKupaKupa/Form1.cs
+++ KupaKupaKupa/Form1.cs
@@ -10,8 +10,19 @@ using System.Windows.Forms;
 
 namespace KupaKupaKupa
 {
+
     public partial class Form1 : Form
     {
+        //????
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
+                return cp;
+            }
+        }
         private bool toKupaOrNotToKupa = false;
         Bitmap bitmap;
         public Form1()
